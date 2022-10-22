6a492a88f13b4e883c00df2368d1f2a98de2d3e4
huxiaoge <2536559210@qq.com>
2022-10-22 18:43:18 +0800

    第一次提交

---
+++ demo1.py
@@ -0,0 +1,19 @@
+# 我一定会好好学习，认真对待研究生生涯中的每一个挑战，尽全力完成好每一项科研工作！
+# 姓名：胡小格
+# 开发时间：2022/10/2 16:04
+class Car:
+    def __init__(self, brand):  # 初始化方法
+        self.brand = brand
+
+    def start(self):  # 实例方法
+        print('汽车已启动...')
+
+
+car = Car('宝马X5')
+car.start()
+print(car.brand)
+
+
+
+
+
